6c26211172d3f87ad920081c767a8b05a08d22ab
adifsgaid <infosgaid@gmail.com>
2021-05-12 13:45:20 +0200

    Validation Emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validates  :email, :name, :password, presence: true
+    validates  :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Must be a valid format" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_110115) do
+ActiveRecord::Schema.define(version: 2021_05_12_105421) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "password_digest"
+    t.string "name", null: false
+    t.string "password_digest", null: false
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "email"
   end
 
 end
