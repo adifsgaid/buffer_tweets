73630665f4cfd8a807161fc551fcc29bf117af39
adifsgaid <infosgaid@gmail.com>
2021-05-14 22:42:59 +0200

    create us

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,17 @@ class RegistrationsController < ApplicationController
     end
 
     def create
-        
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path, notice: "Successfully created account"
+        else
+            render :new
+        end
     end
-end
+
+    
+    private
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+end 
