65ed61a9621628c0390ff73dace9bc78bc8488f5
adifsgaid <infosgaid@gmail.com>
2021-05-17 09:32:45 +0200

    conf

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   
   get 'sign_up', to: 'registrations#new', as: :sign_up
   post 'sign_up', to: 'registrations#create'
+  delete 'log_out', to: 'session#destroy'
   
   root to: 'main#index'
 end
