f35cab217fcf169f71e99a392ff3b68a4f61c06b
adifsgaid <infosgaid@gmail.com>
2021-06-21 09:00:00 +0200

    Token auth

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -8,4 +8,24 @@ class PasswordResetsController < ApplicationController
 
     redirect_to root_path, notice: 'An email has been sent, click the link to reset your password.'
   end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: 'Password_Reset')
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: 'Your token has expired, please try again'
+  end
+
+  def update
+    if Current.user.update(password_params)
+      redirect_to root_path, notice: 'Password Sucesfully Changed'
+    else
+      render :edit, alert: 'Something Went Wrong'
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
