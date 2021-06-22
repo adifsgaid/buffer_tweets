33da8195d30ffe7ac7bae2b0796583ef6a636c80
adifsgaid <infosgaid@gmail.com>
2021-06-22 07:55:59 +0200

    added mailer and resetter

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,20 +1,19 @@
 class PasswordsController < ApplicationController
-    before_action :require_logged_in
+  before_action :require_logged_in
 
-    def edit
+  def edit; end
+
+  def update
+    if Current.user.update(password_params)
+      redirect_to root_path, notice: 'Password Sucesfully Changed'
+    else
+      render :edit, alert: 'Something Went Wrong'
     end
+  end
 
-    def update
-        if Current.user.update(password_params)
-             redirect_to root_path, notice: "Password Sucesfully Changed"
-        else
-            render :edit, alert: "Something Went Wrong"
-        end
-    end
+  private
 
-    private
-
-    def password_params
-        params.require(:user).permit(:password, :password_confirmation)
-    end
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
